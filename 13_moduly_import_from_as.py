83c495797a1beb217608bc9f17041c2105dae460
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2020-09-24 19:36:40 +0200

    The file my_big_module.py has been translated to English.

--- 13_moduly_import_from_as.py
+++ 13_moduly_import_from_as.py
@@ -1,21 +1,23 @@
-#Pamietamy użycie import math, import time i import datetime.
-#Importowaliśmy wtedy moduły by móc wykorzystać napisane przez kogoś funkcje.
-#Możemy też pisać własne moduły
+# Pamietamy użycie import math, import time i import datetime.
+# Importowaliśmy wtedy moduły by móc wykorzystać napisane przez kogoś funkcje.
+# Możemy też pisać własne moduły
 
 
-#Importujemy moduł
+# Importujemy moduł
 import moj_modul
-#używamy funkcji za pomocą składni nazwa_modulu.nazwa_funkcji()
+# używamy funkcji za pomocą składni nazwa_modulu.nazwa_funkcji()
 print("2+2 =", moj_modul.dodaj(2,2))
 
 
-#Jeśli z dużego modułu potrzebuję tylko kilku funkcji
-from moj_duzy_modul import iloczyn,iloraz
-#Tutaj nie trzeba pisać nazwy modułu!
-print("2*3 =", iloczyn(2,3))
+# Jeśli z dużego modułu potrzebuję tylko kilku funkcji
+from my_big_module import calculate_product, calculate_quotient
+# Tutaj nie trzeba pisać nazwy modułu!
+print("2*3 =", calculate_product(2, 3))
 
 
-#Można ustalać swoje nazwy funkcji jeśli oryginalne są kłopotliwe
+# Można ustalać swoje nazwy funkcji jeśli oryginalne są kłopotliwe
 from datetime import datetime as data
 print(data.now())
-#wcześniej datetime.datetime.now()
+# wcześniej datetime.datetime.now()
+
+# Jest dobra praktyka umieszczac importy modulow na poczatku pliku
---
+++ my_big_module.py
@@ -0,0 +1,17 @@
+variable_in_my_module = 2.74
+
+
+def calculate_sum(a, b):
+    return a + b
+
+
+def calculate_the_difference(a, b):
+    return a - b
+
+
+def calculate_product(a, b):
+    return a * b
+
+
+def calculate_quotient(a, b):
+    return a / b
