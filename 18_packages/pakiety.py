0ffceea8ed5aee738863ba212f2f8317c54072f0
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2021-05-13 00:16:01 +0200

    More clean up. WiP

--- 18_packages/pakiety.py
+++ 18_packages/pakiety.py
@@ -4,17 +4,22 @@
 
 #Dodano pakiet my_package oraz jego podpakiet my_sub_package.
 #Pliki __init__.py zostały dodane automatycznie. Są one puste (i mogą takie być).
+def main():
+    import my_package.some_main_script
 
-import my_package.some_main_script
+    napis = my_package.some_main_script.konkat("Ala ", "ma kota")
+    # The PyCharm may highlight some_main_script because of incorrect naming convention in 18_packages
+    print(napis)
 
-napis = my_package.some_main_script.konkat("Ala ", "ma kota")
-print(napis)
+    from my_package.my_sub_package.some_sub_script import fun as fun
+    fun()
+    #fun2()  #NameError: name 'fun2' is not defined
 
-from my_package.my_sub_package.some_sub_script import fun as fun
-fun()
-#fun2()  #NameError: name 'fun2' is not defined
+    #from my_package.my_sub_package.some_sub_script import *
+    fun()
+    #Można wywołać fun2 (L20) bo wszystko jest zaimportowane. Nawet bez linii 17
+    my_package.my_sub_package.some_sub_script.fun2()
 
-#from my_package.my_sub_package.some_sub_script import *
-fun()
-#Można wywołać fun2 (L20) bo wszystko jest zaimportowane. Nawet bez linii 17
-my_package.my_sub_package.some_sub_script.fun2()
+
+if __name__ == '__main__':
+    main()
