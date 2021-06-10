b8730196a4b7d2a05410f3967b735c8de275c7c4
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2021-06-10 23:52:49 +0200

    Lesston about comparing objects has been added. WiP

---
+++ magic_methods/comparing_objects.py
@@ -0,0 +1,40 @@
+# Let's say that you want to compare cars (or any other objects).
+class Car:
+    def __init__(self, car_brand: str, engine_capacity: float, color: str):
+        self.car_brand = car_brand
+        self.engine_capacity = engine_capacity
+        self.color = color
+
+    # To do that, you have to tell the Python how to compare objects.
+    # You do that, by defining a method called eq.
+    def __eq__(self, other):
+
+        # If both instances belong to the same class, then you may compare their fields.
+        if isinstance(self, other.__class__):
+            # Let's say that you care about a brand and capacity but do not care about the color.
+            # Therefore, write the following:
+            return self.car_brand == other.car_brand and self.engine_capacity == other.engine_capacity
+
+        else:
+            # If the instances are not of the same class the objects are not equal.
+            return False
+
+
+# Now, let's add another simple class.
+class Bicycle:
+    def __init__(self, color):
+        self.color = color
+
+
+if __name__ == "__main__":
+    # Now let assume that you have a 2 bikes and 3 cars.
+    bike = Bicycle("blue")
+    bike2 = Bicycle("blue")
+    toyota = Car("Toyota", 1.8, "grey")
+    mc1 = Car("Mercedes", 1.8, "grey")
+    mc2 = Car("Mercedes", 1.8, "red")
+
+    # Everyone knows that a bicycle is not a car ;)
+    print("Q: Is a bike a car? A:", bike == toyota)
+
+
