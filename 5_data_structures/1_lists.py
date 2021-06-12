0ae9847690aeabdeb1d2a66af65e606e1b86097d
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2021-06-12 20:37:10 +0200

    A lesson about lists has been upgraded.

--- 5_data_structures/1_lists.py
+++ 5_data_structures/1_lists.py
@@ -10,62 +10,43 @@ print("my_list: ", my_list)
 # You may access an element of list with the following line:
 print("my_list[0]: ", my_list[0])
 
-
+# Since all items stored in our list are strings, you can access letters with:
 print("my_list[0][0]: ", my_list[0][0])
 
-print(my_list[:2])
+# A list can be sliced
+print("my_list[:2]: ", my_list[:2])
 
-print("-----------------------")
+# Lists are mutable, this means that once created a list may be changed.
+my_list[0] = "item 4"
+print("\nmy_list with changed item: ", my_list)
+
+# However an attempt to add an item to list:
+# my_list[3] = "item 4"
+# will result in an IndexError with the message: list assignment index out of range
+# To add an additional item to list you have to use a method append
+
+# The variable my_list stores a reference to an object
 my_list2 = my_list
-print("my_list: ", my_list)
+print("\nmy_list: ", my_list)
 print("my_list2: ", my_list2)
 
-my_list2[0] = "A new item"
-print("my_list: ", my_list)
+my_list[0] = "A new item"
+print("my_list after changing first item: ", my_list)
 print("my_list2: ", my_list2)
+# To copy values instead of reference to a new variable you may use a method copy.
 
-print("copy.copy(lista) -----------------------")
-import copy
-lista = ["ser", "mleko", "parowki"]
-lista2 = copy.copy(lista)
-print(lista)
-print(lista2)
+# Unlike in C++ or in Java a list may store data of different types
+my_list = [1, 2.5, "item"]
+print("\nmy_list: ", my_list)
 
-lista.clear()
-print(lista)
-print(lista2)
-
-print("-----------------------")
-lista = ["ser", "mleko", "parowki"]
-lista2 = lista.copy()
-print(lista)
-print(lista2)
-
-lista.clear()
-print(lista)
-print(lista2)
-
-print("-----------------------")
-lista = [1, 2.5, "parowki"]
-print(lista)
-
-print("lista.insert(index, object) -----------------------")
-lista.insert(2, 3.14)
-print(lista)
-
-print("-----------------------")
-print(lista.pop(2))
-print(lista)
-
-print("-----------------------")
-li1 = [0,1,2]
-li2 = [3,4,5]
-li3 = [6,7,8]
-
+# A list may contain other data structures like lists, dictionaries etc.
+li1 = [0, 1, 2]
+li2 = [3, 4, 5]
+li3 = [6, 7, 8]
 li = [li1, li2, li3]
-print(li)
-print(len(li))
+print("li: ", li)
+print("A length of this list is: ", len(li))
 
-print("-----------------------")
-lista = [1, 1, 1]
-print(lista)
+# List may hold non-unique values:
+my_list = [1, 1, 1]
+print("\nMy non-unique list: ", my_list)
--- 5_data_structures/2_list_methods.py
+++ 5_data_structures/2_list_methods.py
@@ -15,3 +15,32 @@ print(my_list2)
 my_list2.clear()
 print(my_list2)
 print(my_list2)
+
+print("copy.copy(lista) -----------------------")
+import copy
+lista = ["ser", "mleko", "parowki"]
+lista2 = copy.copy(lista)
+print(lista)
+print(lista2)
+
+lista.clear()
+print(lista)
+print(lista2)
+
+print("-----------------------")
+lista = ["ser", "mleko", "parowki"]
+lista2 = lista.copy()
+print(lista)
+print(lista2)
+
+lista.clear()
+print(lista)
+print(lista2)
+
+print("lista.insert(index, object) -----------------------")
+lista.insert(2, 3.14)
+print(lista)
+
+print("-----------------------")
+print(lista.pop(2))
+print(lista)
